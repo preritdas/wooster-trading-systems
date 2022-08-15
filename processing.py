14b7c540596a5e31556f8ed0957d2a84bce8d863
Prerit Das <preritdas@gmail.com>
2022-08-15 12:38:39 -0400

    Optimize systems internally.

--- processing.py
+++ processing.py
@@ -24,9 +24,11 @@ current_dir = os.path.dirname(
 
 def _process_system(
     strategy: bt.Strategy, 
-    name: str, 
+    name: str,
+    index: int,
     data: pd.DataFrame,
-    optimize: bool = False
+    optimize: bool,
+    optimizer: str
 ) -> tuple[pd.Series, str]:
     """
     Base processor. Backtests, stores performance website, 
@@ -44,15 +46,21 @@ def _process_system(
     stats = backtest.run()
 
     if optimize:
-        stats = backtest.optimize()
-    
+        optimizers = systems.systems[index][1].Params.optimizers
+        optimizers["maximize"] = optimizer
+        stats = backtest.optimize(**optimizers)
+
     filename = os.path.join(current_dir, f"results/{name}.html")
     backtest.plot(filename=filename, open_browser=False)
 
     return stats, filename
 
 
-def process_system_idx(index: int, optimize: bool = False) -> tuple[pd.Series, str]:
+def process_system_idx(
+    index: int, 
+    optimize: bool, 
+    optimizer: str
+) -> tuple[pd.Series, str]:
     """
     Find and process a system by its given index.
 
@@ -67,13 +75,15 @@ def process_system_idx(index: int, optimize: bool = False) -> tuple[pd.Series, s
     return _process_system(
         strategy = system[2],
         name = system[0],
+        index = index,
         data = utils.data(
             symbol = system[1].Params.symbol,
             interval = system[1].Params.timeframe,
             start = system[1].Params.start,
             end = system[1].Params.end
         ),
-        optimize = optimize
+        optimize = optimize,
+        optimizer = optimizer
     )
 
 
@@ -83,28 +93,8 @@ def process_latest_system() -> tuple[pd.Series, str]:
 
     Returns a tuple containing the results series and a string path 
     to the interactive plot.
+
+    Deprecate this or build its functionality into process_system_idx.
+    if idx is greater than the max, idx = the max or smth.
     """
     return process_system_idx(index=max(systems.systems))
-
-
-# ---- Strategy parameter optimization ----
-
-def _optimize_system_idx(index: int) -> bt.Backtest:
-    """
-    Optimize strategy and return a backtest object.
-    """
-    system = systems.systems[index]
-
-    backtest = bt.Backtest(
-        data = utils.data(
-            system[1].Params.symbol,
-            system[1].Params.timeframe,
-            system[1].Params.start,
-            system[1].Params.end
-        ),
-        strategy = system[2],
-        cash = 100_000
-    )
-
-    backtest.optimize()
-    return backtest
