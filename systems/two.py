fe810168475ead0402f4d87510043c7ca6d747c5
Prerit Das <preritdas@gmail.com>
2022-08-16 23:19:04 -0400

    Designed Wooster Two.

--- systems/two.py
+++ systems/two.py
@@ -3,20 +3,29 @@ Wooster Two.
 """
 # External imports
 import backtesting as bt
+import talib as ta
+
+# Local imports
+import datetime as dt
 
 
 class Params:
     """
     Param template for Wooster Two.
     """
-    symbol = None
-    timeframe = None
-    period = None
-    start = None
-    end = None
+    symbol = "AAPL"
+    timeframe = "2m"
+    period = "60d"
+    start = dt.datetime.today() - dt.timedelta(days=55)
+    end = dt.datetime.today() - dt.timedelta(days=1)
 
     optimizers: dict = {
-        "constraint": lambda params: True
+        "rsi_period": range(3, 30, 1),
+        "buy_rsi_entry": range(5, 49, 1),
+        "buy_rsi_target": range(5, 95, 1),
+        "sell_rsi_entry": range(51, 95, 1),
+        "sell_rsi_target": range(5, 95, 1),
+        "constraint": lambda params: params.buy_rsi_entry < params.buy_rsi_target and params.sell_rsi_entry > params.sell_rsi_target
     }
 
 
@@ -24,16 +33,31 @@ class WoosterTwo(bt.Strategy):
     """
     Godspeed, Wooster Two.
 
-    
+    Same as Wooster One but sell the position if RSI crosses 50, from
+    either direction.
     """
-    indicator = 123
+    rsi_period = 14
+    buy_rsi_entry = 20
+    buy_rsi_target = 50
+    sell_rsi_entry = 80
+    sell_rsi_target = 50
 
     def init(self):
         """
         Indicators etc.
         """
+        self.rsi = self.I(ta.RSI, self.data.Close, self.rsi_period)
 
     def next(self):
         """
         Trading logic.
         """
+        # Position management
+        if self.position.is_long:  # check for buy target
+            if self.rsi[0] >= self.buy_rsi_target: self.position.close()
+        if self.position.is_short:
+            if self.rsi[0] <= self.sell_rsi_target: self.position.close()
+        
+        # New positions
+        if self.rsi[0] < self.buy_rsi_entry: self.buy()
+        elif self.rsi[0] > self.sell_rsi_entry: self.sell()
