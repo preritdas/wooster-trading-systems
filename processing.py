b9d374d69206ba31aa604f3c388f946e0fce6587
Prerit Das <preritdas@gmail.com>
2022-08-18 15:42:52 -0400

    Check train up down chop dict data structure in system processing.

--- processing.py
+++ processing.py
@@ -34,7 +34,7 @@ def _process_system(
     strategy: bt.Strategy, 
     name: str,
     index: int,
-    data: pd.DataFrame,
+    data: pd.DataFrame | dict[str, pd.DataFrame],
     optimize: bool,
     optimizer: str,
     method: str,
@@ -44,16 +44,28 @@ def _process_system(
     Base processor. Backtests, stores performance website, 
     and returns results series.
 
+    If data is passed in as a dict, keys must be "train", "up", "down", and "chop."
+    Each key's value must be a DataFrame. If a dict is passed and all four of these
+    keys are not present with their correct values, a ValueError is raised.
+
     Returns a tuple containing the results series and a string path 
     to the interactive plot.
     """
+    # If dict, ensure data is labeled properly with DataFrame data
+    if isinstance(data, dict) and optimize:
+        assert "train" in data and "up" in data and "down" in data and "chop" in data
+        assert isinstance(data["train"], pd.DataFrame) \
+            and isinstance(data["up"], pd.DataFrame) \
+            and isinstance(data["down"], pd.DataFrame) \
+            and isinstance(data["chop"], pd.DataFrame)
+
     backtest = bt.Backtest(
         data = data,
         strategy = strategy,
         cash = 100_000
     )
 
-    stats = backtest.run(show_progress=True)
+    stats = backtest.run(show_progress=progress)
 
     if optimize:
         optimizers = systems.systems[index][1].Params.optimizers
@@ -69,7 +81,7 @@ def _process_system(
     backtest.plot(filename=plotpath, open_browser=False)
 
     # Reset the page title so it's not the filename
-    time.sleep(1)  # experimental: does this trigger OSError 22? 
+    time.sleep(1)  # Prevent OS error 22
     kit.append_by_query(
         query = "<title>",
         content = f"\t\t<title>{name}</title>",
