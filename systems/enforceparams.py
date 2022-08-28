8b959890afe08596ef68a2989d24ccd2599a5ac4
Prerit Das <preritdas@gmail.com>
2022-08-28 17:45:01 -0400

    New module to enforce a system's Params class.
    `params_structure` is a dictionary with string keys and anonymous function values. When the function is called with an input of the attribute, the function returns a boolean of whether or not the attribute conforms to the style, value, and configuration necessary.
    
    `_check_system` is given a Params class and an index, and iteratively checks all parameters using the `params_structure` dictionary mentioned above.

---
+++ systems/enforceparams.py
@@ -0,0 +1,56 @@
+"""
+Check a system's Params class to ensure it has all the required, properly structured
+information. If it doesn't, the whole processing pipeline is thrown off. Eventually,
+explore replacing this with Pydantic.
+"""
+import datetime as dt
+import config
+import utils
+
+
+# ---- Necessary structure ----
+
+params_structure = {
+    "symbol": lambda attr: isinstance(attr, str),
+    "timeframe": lambda attr: isinstance(attr, str) \
+        and attr in {"1m", "5m", "15m", "30m", "60m", "1d", "1w"},
+    "filter_eod": lambda attr: isinstance(attr, bool),
+    "walkforward": lambda attr: isinstance(attr, dict) \
+        and all([isinstance(key, str) for key in attr.keys()]) \
+        and all([key in {"train", "up", "down", "chop"} for key in attr.keys()]) \
+        and all([isinstance(val, tuple) for val in attr.values()]) \
+        and all([isinstance(val, dt.date) or isinstance(dt.datetime) \
+            for tups in attr.values() for val in tups]),
+    "optimizers": lambda attr: isinstance(attr, dict) \
+        and all([isinstance(key, str) for key in attr.keys()])
+}
+
+
+def _check_system(params_class, idx: int) -> None:
+    """
+    Checks the class. If an error is found, a fatal exception or assertion error 
+    is thrown.
+    """
+    # Ensure parameter enforcement is enabled in config
+    if not config.Systems.parameter_enforcement: return
+
+    assert isinstance(idx, int)
+    attrs = params_class.__dict__
+
+    error_prefix = f"System {idx} parameter enforcement error:"
+    error_style = "bold red"
+
+    for attr, check in params_structure.items():
+        if not attr in attrs:
+            utils.console.log(
+                f"{error_prefix} Attribute '{attr}' not in system {idx} Params class.",
+                style = error_style
+            )
+            quit()
+        if not check(attrs[attr]):
+            utils.console.log(
+                f"{error_prefix} "
+                f"System {idx} Params attribute '{attr}' failed its structural test.",
+                style = error_style
+            )
+            quit()
