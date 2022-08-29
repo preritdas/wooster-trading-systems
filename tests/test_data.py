efcbbd902bd98b509eeda990ac7b290312159eef
Prerit Das <preritdas@gmail.com>
2022-08-29 18:23:53 -0400

    Test incremental data aggregation.

---
+++ tests/test_data.py
@@ -0,0 +1,12 @@
+import data
+import datetime as dt
+
+
+def test_finnhub_aggregation():
+    data._incremental_aggregation(
+        "AAPL",
+        "1m",
+        dt.date(2019, 1, 5),
+        dt.date(2021, 1, 1),
+        filter_eod = True
+    )
