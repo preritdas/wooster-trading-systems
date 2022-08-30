ecf62dc75c99f10169ed597f02a12048f43ebd69
Prerit Das <preritdas@gmail.com>
2022-08-30 01:20:46 -0400

    Wrote tests for initializing and loading data cache.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,9 @@
+"""
+Test data aggregation and caching.
+"""
 import data
 import datetime as dt
+import pandas as pd  # type checking
 
 
 def test_finnhub_aggregation():
@@ -10,3 +14,19 @@ def test_finnhub_aggregation():
         dt.date(2020, 1, 1),
         filter_eod = True
     )
+
+
+def test_data_cache():
+    # Test init
+    n_bars = data.init_cache("MSFT", "1m", 1)
+    assert isinstance(n_bars, int)
+
+    # Test fetch
+    cache_registry = data._fetch_cache()
+    assert isinstance(cache_registry, pd.DataFrame)
+
+    # Test load
+    start = dt.date.today() - dt.timedelta(days=20)
+    end = dt.date.today() - dt.timedelta(days=2)
+    data_loaded = data.load_cache("MSFT", "1m", start, end)
+    assert not data_loaded.empty
