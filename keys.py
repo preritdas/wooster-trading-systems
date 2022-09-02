847fcb10df063a6a0991b0022a47cceeb099f514
Prerit Das <preritdas@gmail.com>
2022-09-02 16:28:52 -0400

    Standalone key reader file.

---
+++ keys.py
@@ -0,0 +1,38 @@
+"""
+Read keys.ini file.
+"""
+import configparser
+import os
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+keys = configparser.RawConfigParser()
+keys.read(os.path.join(current_dir, "keys.ini"))
+
+
+class Finnhub:
+    """
+    Market data.
+    """
+    api_key = keys["Finnhub"]["api_key"]
+
+
+class Nexmo:
+    """
+    Sending text alerts. Optional.
+    """
+    try:
+        nexmo_keys = keys["Nexmo"]
+    except ValueError:
+        nexmo_keys = None
+
+    if nexmo_keys is None:
+        api_key = None
+        api_secret = None
+        sender = None
+        receiver = None
+    else:
+        api_key = nexmo_keys["api_key"]
+        api_secret = nexmo_keys["api_secret"]
+        sender = nexmo_keys["sender"]
+        receiver = nexmo_keys["receiver"]
