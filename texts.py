13d729b42abc364159e51384a7f5d70f4b2ad02e
Prerit Das <preritdas@gmail.com>
2022-09-02 16:29:19 -0400

    Rely on standalone keys module.

--- texts.py
+++ texts.py
@@ -4,54 +4,44 @@ of a processing/optimization operation. Keys do not have to be provided. If they
 are not, the text_me function simply does nothing, without returning any errors,
 thereby preventing a program crash.
 """
+# External imports
 import nexmo
 
-import configparser
-import os
+# Interal imports
 import warnings
 
-
-current_dir = os.path.dirname(
-    os.path.realpath(__file__)
-)
-
-
-keys = configparser.RawConfigParser()
-keys.read(os.path.join(current_dir, "keys.ini"))
+# Project modules
+import keys
 
 
 def _keys_given() -> bool:
     """
     Determines whether or not all keys have been given for Nexmo.
     """
-    fields = "api_key", "api_secret", "sender", "receiver"
-
-    try:
-        nexmo_keys = keys["Nexmo"]
-    except KeyError:
+    if keys.Nexmo.nexmo_keys is None: 
         return False
-
-    for field in fields:
-        try: 
-            if not nexmo_keys[field]: return False
-        except KeyError: 
-            return False
 
     return True
 
 
 def _auth_nexmo() -> tuple[nexmo.Client, nexmo.Sms]:
-    client = nexmo.Client(keys["Nexmo"]["api_key"], keys["Nexmo"]["api_secret"])
+    """
+    Only call this if _keys_given returns True.
+    """
+    client = nexmo.Client(keys.Nexmo.api_key, keys.Nexmo.api_secret)
     return client, nexmo.Sms(client)
 
 
-def text_me(message: str) -> None:
+def text_me(message: str) -> bool:
+    """
+    Return False if no text is sent due to no keys, True if text was sent.
+    """
     # Silently return if no keys given to prevent crash
     if not _keys_given(): 
         warnings.warn(
             "texts.text_me was called but no Nexmo keys were given in keys.ini."
         )
-        return
+        return False
 
     # Assuming keys are given
     client, sms = _auth_nexmo()
@@ -63,3 +53,5 @@ def text_me(message: str) -> None:
             "text": str(message)
         }
     )
+
+    return True
