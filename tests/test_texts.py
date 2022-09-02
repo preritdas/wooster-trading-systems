6eed990fd3cc018fcbb8116b85f46ea0495a3ab9
Prerit Das <preritdas@gmail.com>
2022-09-02 16:29:49 -0400

    Test no keys with mock values.

--- tests/test_texts.py
+++ tests/test_texts.py
@@ -1,8 +1,9 @@
-import texts
-import nexmo
+import pytest
 
 
 def test_keys():
+    import texts
+
     keys_given = texts._keys_given()
     assert isinstance(keys_given, bool)
     
@@ -11,3 +12,13 @@ def test_keys():
         client, sms = texts._auth_nexmo()
         client.get_account_numbers()
         
+
+def test_handle_no_keys(mocker):
+    mocker.patch('keys.Nexmo.nexmo_keys', None)
+    import texts
+
+    assert not texts._keys_given()
+
+    with pytest.warns(UserWarning):
+        assert not texts.text_me("This should not send.")
+    
